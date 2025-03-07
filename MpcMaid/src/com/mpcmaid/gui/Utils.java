e3da408a84d77e850bc6f33cde806700b2d0482c
Chaiyong Ragkhitwetsagul <cragkhit@gmail.com>
2025-03-07 09:15:45 +0700

    Update Utils.java
    Added `try-finally` to properly close the input and output streams.

--- MpcMaid/src/com/mpcmaid/gui/Utils.java
+++ MpcMaid/src/com/mpcmaid/gui/Utils.java
@@ -93,16 +93,21 @@ public class Utils {
 	// File utils
 	public final static void copy(File src, File dst) throws IOException {
 		InputStream in = new FileInputStream(src);
-		OutputStream out = new FileOutputStream(dst);
-
-		// Transfer bytes from in to out
-		byte[] buf = new byte[1024];
-		int len;
-		while ((len = in.read(buf)) > 0) {
-			out.write(buf, 0, len);
-		}
-		in.close();
-		out.close();
+    		try {
+        		OutputStream out = new FileOutputStream(dst);
+        		try {
+            			// Transfer bytes from in to out
+            			byte[] buf = new byte[1024];
+            			int len;
+            			while ((len = in.read(buf)) > 0) {
+                			out.write(buf, 0, len);
+            			}
+        		} finally {
+            			out.close();
+        		}
+    		} finally {
+        		in.close();
+    		}
 	}
 
 	public String toString() {
